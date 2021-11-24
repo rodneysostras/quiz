975a27b66d55aaf6d5eb0a5b4ee6c410b919c326
Rodney Sostras <rodney.sostras@gmail.com>
2021-11-24 10:48:54 -0300

    Start project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
-// │ Copyright (c) 2021 by the author of the test-waproject-01 project. All rights reserved.      │
+// │ Copyright (c) 2021 by the author of the Quiz project. All rights reserved.                   │
 // │ This owner-supplied source code has no limitations on the condition imposed on the           │
 // │ maintenance of the copyright notice.                                                         │
 // │ For more information, read the LICENSE file at the root of the project.                      │
 // │ Written by author Rodney Sostras <rodney.sostras@gmail.com>.                                 │
 // └──────────────────────────────────────────────────────────────────────────────────────────────┘
 
-import * as React from 'react';
+import * as React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            {/* O resto da sua aplicação */}
+        </React.Fragment>
+    );
 }
