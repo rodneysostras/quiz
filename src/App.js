32ddd2662c10633bd1b7f1165b2f9a154f5934ff
Rodney Sostras <rodney.sostras@gmail.com>
2021-11-24 00:27:55 -0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ Copyright (c) 2021 by the author of the test-waproject-01 project. All rights reserved.      │
+// │ This owner-supplied source code has no limitations on the condition imposed on the           │
+// │ maintenance of the copyright notice.                                                         │
+// │ For more information, read the LICENSE file at the root of the project.                      │
+// │ Written by author Rodney Sostras <rodney.sostras@gmail.com>.                                 │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+import * as React from 'react';
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ Copyright (c) 2021 by the author of the test-waproject-01 project. All rights reserved.      │
+// │ This owner-supplied source code has no limitations on the condition imposed on the           │
+// │ maintenance of the copyright notice.                                                         │
+// │ For more information, read the LICENSE file at the root of the project.                      │
+// │ Written by author Rodney Sostras <rodney.sostras@gmail.com>.                                 │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘ 
+
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './theme/globalstyle.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
