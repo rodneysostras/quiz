02038643e9749c9027d8b917df2fd71ff4fd35fc
Rodney Sostras <rodney.sostras@gmail.com>
2021-12-01 23:30:03 -0300

    feat: question context provider
    	new file:   src/context/quiz-question/index.jsx
        new file:   src/index.js

---
+++ src/context/quiz-question/index.jsx
@@ -0,0 +1,33 @@
+// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ Copyright (c) 2021 by the author of the Quiz project. All rights reserved.                   │
+// │ This owner-supplied source code has no limitations on the condition imposed on the           │
+// │ maintenance of the copyright notice.                                                         │
+// │ For more information, read the LICENSE file at the root of the project.                      │
+// │ Written by author Rodney Sostras <rodney.sostras@gmail.com>.                                 │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+import * as React from "react";
+
+export const QuizQuestionsContext = React.createContext();
+
+export function QuizQuestionsContextProvider({ children }) {
+    const quizQuestionsStore =
+        localStorage.getItem("quiz-questions") ||
+        '{"current":0, "questions": []}';
+
+    const [quizQuestions, setQuizQuestions] = React.useState(
+        JSON.parse(quizQuestionsStore)
+    );
+
+    React.useMemo(() => {
+        localStorage.setItem("quiz", JSON.stringify(quizQuestions));
+    }, [quizQuestions]);
+
+    return (
+        <QuizQuestionsContext.Provider value={[quizQuestions, setQuizQuestions]}>
+            {children}
+        </QuizQuestionsContext.Provider>
+    );
+}
+
+export default QuizQuestionsContext;
