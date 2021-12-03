1d279460a1bcbb690c277ef6d57f3eea9b9a1933
Rodney Sostras <rodney.sostras@gmail.com>
2021-12-03 14:45:48 -0300

    feat: Refractory Resource to Component

---
+++ src/components/question-title/index.jsx
@@ -0,0 +1,21 @@
+//  ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+//  │ Copyright (c) 2021 by the author of the Quiz project. All rights reserved.                   │
+//  │ This owner-supplied source code has no limitations on the condition imposed on the           │
+//  │ maintenance of the copyright notice.                                                         │
+//  │ For more information, read the LICENSE file at the root of the project.                      │
+//  │ Written by author Rodney Sostras <rodney.sostras@gmail.com>.                                 │
+//  └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+import { Typography } from "@material-ui/core";
+import * as React from "react";
+import useStyles from "./styled";
+
+export default function QuestionTitle({ index, title }) {
+    const classes = useStyles();
+    return (
+        <Typography variant="h6" className={classes.title}>
+            <span>{index} - </span>
+            <b dangerouslySetInnerHTML={{ __html: title }} />
+        </Typography>
+    );
+}
---
+++ src/components/question-title/styled.js
@@ -0,0 +1,19 @@
+// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ Copyright (c) 2021 by the author of the Quiz project. All rights reserved.                   │
+// │ This owner-supplied source code has no limitations on the condition imposed on the           │
+// │ maintenance of the copyright notice.                                                         │
+// │ For more information, read the LICENSE file at the root of the project.                      │
+// │ Written by author Rodney Sostras <rodney.sostras@gmail.com>.                                 │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(() => ({
+    title: {
+        fontSize: "1rem",
+        fontWeight: 400,
+        lineHeight: 1,
+    },
+}));
+
+export default useStyles;
